d28a60039163d305cf8ae55e526f58efad54dc9e
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2024-08-30 20:03:35 +0700

    catalog

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
-    <div className="w-72 h-80 border-2 m-2 rounded-md relative">
-      <div id="card-image">
-        <img src="" alt="" />
+    <div className="w-72 h-96 border-2 m-2 rounded-md relative">
+      <div id="card-image" className="w-64 h-64 mx-auto m-2 ">
+        <img
+          src={`http://localhost:8080/${data.gambar_product}`}
+          className="rounded-md mx-auto h-52"
+          alt=""
+        />
       </div>
       <div id="card-text">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
-          voluptas!
+        <p>{data.deskripsi_product}</p>
+        <p className="font-semibold text-red-600">
+          Price <span className="text-black">Rp.{data.harga}</span>
         </p>
-        <p className="font-semibold text-red-600">Price</p>
-        <button className="bg-blue-500 w-full text-white font-semibold p-1 absolute bottom-0">
+
+        <button className="bg-blue-500 w-full text-white font-semibold  absolute bottom-0">
           Buy Now
         </button>
       </div>
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Filter from "../components/Filter";
+import CatalogueAPI from "../api/CatalogueAPI";
 const Catalog = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await CatalogueAPI.GetCatalogue();
+    setData(response.products);
+    console.log(response);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="flex justify-center w-screen">
       <div className="w-3/4">
         <Banner />
         <Filter />
-        <Card />
+        <div className="grid grid-cols-3 gap-2">
+          {data?.map((item) => (
+            <Card data={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
